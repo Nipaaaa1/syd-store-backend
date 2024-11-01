1ba4a8a17a4b995c467d6e8079f3227816430c4a
Nipaaaa1 <frostone224@gmail.com>
2024-11-01 12:56:18 +0700

    feat: add middleware authorization

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,31 @@ import { db } from './db/index.ts'
 import { usersTable } from './db/schema.ts'
 import { logger } from 'hono/logger';
 import auth from './routes/auth.ts';
+import { jwt, type JwtVariables } from 'hono/jwt';
+import { env } from 'hono/adapter';
 
-const app = new Hono()
+const app = new Hono<{ Variables: JwtVariables }>()
 
 app.use(logger())
+
+app.use('/items/*', (c, next) => {
+  const { JWT_ACCESS_TOKEN_SECRET } = env(c)
+  const jwtMiddleware = jwt({
+    secret: JWT_ACCESS_TOKEN_SECRET as string
+  })
+
+  return jwtMiddleware(c, next)
+})
 
 app.get('/', async (c) => {
   const user = await db.select().from(usersTable)
   return c.json({
     data: user 
   })
+})
+
+app.get('/items/test', (c) => {
+  return c.text('Berhasil')
 })
 
 app.route('/auth', auth)
