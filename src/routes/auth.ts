77ea8a44dcf9d333f74edfd967dff4619522ba1c
Nipaaaa1 <frostone224@gmail.com>
2024-11-01 07:19:04 +0700

    feat: add jwt payload

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
 import { registerSchema } from "../lib/validator/auth-validator.js";
-import { db } from "../db/index.ts";
+import { db } from "../db/index.js";
 import { usersTable } from "../db/schema.js";
 import { eq } from "drizzle-orm";
 import { hash } from '@node-rs/argon2'
+import { dateInSeconds } from "../lib/utils.js";
+import { sign } from "hono/jwt";
+import { env } from "hono/adapter";
 
 const auth = new Hono()
 
@@ -18,7 +21,7 @@ auth.post('/register', async (c) => {
   const checkDb = await db.select({
     email: usersTable.email
   }).from(usersTable).where(eq(usersTable.email, validatedData.data.email))
-  if(checkDb.length > 0) {
+  if(checkDb) {
     return c.json({
       error: "Email already exists!"
     })
@@ -32,7 +35,21 @@ auth.post('/register', async (c) => {
     name: validatedData.data.name,
     email: validatedData.data.email,
     password_hash: hashedPassword
-  }).returning()
+  }).returning({
+    userId: usersTable.id,
+    userEmail: usersTable.email
+  })
+
+  const accessPayload = {
+    sub: userData[0].userId,
+    email: userData[0].userEmail,
+    exp: dateInSeconds(60 * 15) 
+  }
+
+  const refreshPayload = {
+    sub: userData[0].userId,
+    exp: dateInSeconds(60 * 60 * 24 * 30)
+  }
 
   return c.json({
     success: true,
