a14e89189c0c5f1c26d245b1e114956fdf4a0ce4
Nipaaaa1 <frostone224@gmail.com>
2024-11-07 22:11:28 +0700

    refactor: create insertUser schema
    This create inesrtUser zod schema.

--- src/lib/validator/auth-validator.ts
+++ src/lib/validator/auth-validator.ts
@@ -1,10 +1,18 @@
+import { createInsertSchema } from "drizzle-zod"
 import { z } from "zod"
+import { usersTable } from "../../db/schema.js"
 
 export const registerSchema = z.object({
   name: z.string().max(16),
   email: z.string().email(),
   password: z.string().min(8).max(16)
 })
+
+export const insertUser = createInsertSchema(usersTable, {
+  email: (schema) => schema.email.email()
+})
+
+export type insertUserType = z.infer<typeof insertUser>
 
 export const loginSchema = registerSchema.omit({
   name: true
