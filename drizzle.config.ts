d6d40319e3d4d65586c3b382bd9099add25fa447
Nipaaaa1 <frostone224@gmail.com>
2024-11-11 07:41:50 +0700

    chore: add new migrations
    This makes all migrations placed on ./src/db/migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: './src/db/schema.ts',
+  schema: './src/db/schema.ts', 
   dbCredentials: {
     url: process.env.DATABASE_URL as string
   },
-  verbose: true 
+  verbose: true ,
+  out: './src/db/migrations'
 })
